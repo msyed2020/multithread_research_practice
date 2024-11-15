dde5b8b19203dd840c918dbb65f7130474e05fe9
msyed2020 <67170829+msyed2020@users.noreply.github.com>
2024-11-14 19:21:33 -0500

    Add matrix_mult
    This is NOT finished rn

---
+++ matr_mult.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+typedef struct matrix {
+    int r;
+    int c;
+    int n;
+    int A;
+    int B;
+    int res;
+};
+
+int main() {
+    
+}
